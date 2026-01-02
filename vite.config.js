51213f53006d2c8c7d604bef1f80def6f42166c6
shandyalfa <shandyal2105@gmail.com>
2026-01-02 16:10:22 +0800

    Refactor Vite config to use defineConfig and build settings
    Updated Vite configuration to use defineConfig and added build options.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-// vite.config.js
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/storyshandy/", // nama repo kamu di GitHub
   server: {
     proxy: {
-      '/v1': {
-        target: 'https://story-api.dicoding.dev',
+      "/v1": {
+        target: "https://story-api.dicoding.dev",
         changeOrigin: true,
         secure: true,
       },
     },
   },
-};
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
+});
